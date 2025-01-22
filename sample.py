23b110fea0d661e679ed7f3de4b7ff994c907167
saiKrishna-0311 <krishsai3111@gmail.com>
2025-01-22 22:21:20 +0530

    It is knowing about place aslo in this version.

--- sample.py
+++ sample.py
@@ -1,6 +1,8 @@
 name=input("Please provide your Name: ")
 age=int(input("Please provide your Age: "))
 profession=input("Please Enter your Profession: ")
+place=input("Please Enter where you are from: ")
 print(f"Hello {name}. You are {age} years old.")
-print(f"Profession is {profession}")
-
+print(f"Your profession is {profession}")
+print(f"You are from {place}")
+print("Thankyou")
