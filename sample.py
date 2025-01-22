821283d8bbc14a4cf834c1ca2fe0ed1448348557
saiKrishna-0311 <krishsai3111@gmail.com>
2025-01-22 22:09:28 +0530

    Now it is taking profession as an Input

--- sample.py
+++ sample.py
@@ -1,4 +1,6 @@
 name=input("Please provide your Name: ")
 age=int(input("Please provide your Age: "))
-
+profession=input("Please Enter your Profession: ")
 print(f"Hello {name}. You are {age} years old.")
+print(f"Profession is {profession}")
+
